7c75f61c40f0a164223c189914351d709ca02ad4
Diego <lipchak.diego@gmail.com>
2024-09-04 17:51:24 -0500

    Starting setup

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline"`>
+    Hello World
+  </h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <header class="bg-grey">
+        <nav class="mx-auto flex max-w-7x1 bg-grey-600">
+            <p class="color-red">test</p>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
